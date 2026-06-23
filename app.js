c4375326d1c3c345ecd63f50c20f185ddebdd2cf
AlphaAlejandro9777 <beniitezalejandro97@gmail.com>
2026-06-23 19:43:26 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+particlesJS("particles-js", {
+particles: {
+number: {
+value: 120
+},
+color: {
+value: "#2eff9b"
+},
+shape: {
+type: "circle"
+},
+opacity: {
+value: 0.5
+},
+size: {
+value: 3
+},
+line_linked: {
+enable: true,
+distance: 150,
+color: "#2eff9b",
+opacity: 0.2
+},
+move: {
+enable: true,
+speed: 2
+}
+}
+});
